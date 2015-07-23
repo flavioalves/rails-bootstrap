df593db379550b4a27a67f7c54a91ca5646be50c
Uriel Silva <urielbcs@gmail.com>
2015-07-23 15:54:06 -0300

    facebook login first steps

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < ActionController::Base
   def render_not_found
     render file: "public/404.html", status: 404, layout: false      
   end
+
+  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+	def create
+    user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to root_url
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url
+  end	
+
+  def userparams 
+  	params.require(:user).permit(:provider, :uid, :name, :oauthtoken, :oauthexpiresat) 
+  end
+end
